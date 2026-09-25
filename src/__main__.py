df807b21546f8fd62659347c879113c0c18929fb
nana <135635847+qilinmoe@users.noreply.github.com>
2026-09-25 00:57:51 -0300

    feat: fix twitter/X embeds
    Small feature to fix all twitter/X embeds (notorious for breaking on discord) by responding with a fixed link to one of the multiple services that do this.
    URL customizable via `config.toml` or `x_replace_url` env var.

--- src/__main__.py
+++ src/__main__.py
@@ -1,10 +1,14 @@
+import logging
+logger = logging.getLogger("sunshine-seulgi")
+
+import os, re, httpx
+import hikari, miru, ongaku
+
+from src.utils.embed import *
+from src.utils.deezer import get_result_by_url, get_tracklist_by_query
 from src.utils.config import init_config
 init_config()
 
-import os, httpx
-import hikari, miru, ongaku
-import logging
-logger = logging.getLogger("sunshine-seulgi")
 
 class SunshineSeulgi(hikari.GatewayBot):
     def __init__(self, *args, **kwargs) -> None:
@@ -32,6 +36,17 @@ async def guild_message_create_response(event: hikari.GuildMessageCreateEvent) -
     if not event.is_human or event.channel_id not in allowed_channels:
         return
     me = bot.get_me()
+    content = event.message.content
+    content_clean = str(content).replace(f"<@{os.getenv("discord_bot_id")}> ", "").strip()
+
+    match_x = re.search(re.compile(str(os.getenv("x_replace_regex"))), content_clean)
+    if match_x:
+        async with bot.rest.trigger_typing(event.channel_id):
+            x_link = match_x.group(0)
+            new_match = x_link.replace("https://x.com", str(os.getenv("x_replace_url")))
+            await event.message.respond(new_match, reply=event.message_id, mentions_reply=True)
+        return
+
     if me.id in event.message.user_mentions_ids: # type: ignore
         await event.message.respond("Pong!", reply=event.message_id, mentions_reply=True)
         return
